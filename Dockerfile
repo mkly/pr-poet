c79f5cf58412cc0a20d7cb28d5c26287db223434
Mike Lay <mike@mkly.io>
2023-08-05 15:32:00 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.11-slim
+RUN apt-get update && apt-get install -y curl
+RUN pip --no-cache-dir install ctransformers
+
+WORKDIR /action
+ADD action.py /action
+RUN curl -O -L https://huggingface.co/TheBloke/orca_mini_3B-GGML/resolve/main/orca-mini-3b.ggmlv3.q4_0.bin -o /action/orca-mini-3b.ggmlv3.q4_0.bin
+
+ENV TRANSFORMERS_OFFLINE=1
+
+CMD ["python", "-u", "/action/action.py"]
---
+++ action.py
@@ -0,0 +1,38 @@
+# from github import Github
+from ctransformers import AutoModelForCausalLM, AutoConfig, Config
+import re
+import os
+import sys
+
+config = AutoConfig(
+  model_type='llama',
+  config=Config(
+    max_new_tokens=77,
+  )
+)
+llm = AutoModelForCausalLM.from_pretrained('./orca-mini-3b.ggmlv3.q4_0.bin', config=config)
+
+commit_message = os.getenv('INPUT_COMMIT_MESSAGE')
+
+prompt = f"""
+COMMIT: Allow RSS feed to be cached if user is logged in.
+POEM:
+LINE_ONE: In feeds that dance on screens so bright,
+LINE_TWO: A user logged, enjoys the sight,
+LINE_THREE: Caching RSS, a seamless flow,
+LINE_FOUR: Connection's embrace, a glow to show.
+
+COMMIT: Bring back try/catch for 5.1
+POEM:
+LINE_ONE: In 5.1, a plea to mend,
+LINE_TWO: Bring back try/catch, an old friend,
+LINE_THREE: Errors caught, a graceful dance,
+LINE_FOUR: Code's embrace in a second chance.
+
+COMMIT: {commit_message}
+POEM:
+"""
+
+result = str(llm(prompt))
+lines = result.split("\n")
+poem = re.sub("LINE_ONE: |LINE_TWO: |LINE_THREE: |LINE_FOUR: ", "", "\n".join(lines[:4]))
